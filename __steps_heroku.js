52be0e85779a060398cf49bba8769552fbb39d43
zaforemon20 <abuzaforemon@gmail.com>
2022-05-26 23:11:27 +0600

    adding steps

---
+++ __steps_heroku.js
@@ -0,0 +1,15 @@
+/**
+ * --------------
+ * ONE TIME FOR ANY COMPUTER
+ * --------------
+ * 1. create heroku account
+ * 2. verify email
+ * 3. install heroku cli
+ * 4. run heroku login command in project folder
+ * -----------------------------
+ * For each project one time
+ * =============================
+ * 1. heroku create
+ * 2. make sure you: git add . > git commit > git push
+ * 3. git push heroku main
+ */
